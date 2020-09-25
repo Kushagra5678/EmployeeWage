098aa911f3cb9dedf138e9f2eef50b23909aa44d
Kushagra5678 <kushu.7feb@gmail.com>
2020-09-25 21:26:48 +0530

    UC8 commit

---
+++ employeeWageUC8/src/employeeWageUC8/EmployeeWage.java
@@ -0,0 +1,39 @@
+package employeeWageUC8;
+
+public class EmployeeWage {
+	public static final int full_time=1;
+	public static final int part_time=2;
+	//public static final int emp_rate_per_hour=20;
+	//public static final int working_days=20;
+	//public static final int max_hours=100;
+	
+	public static int computeWage(int emp_rate_per_hour, int working_days, int max_hours) {
+		int empHours=0,totalHours=0;
+		int empWage=0, totalWage=0;
+		int empCheck=(int) Math.floor(Math.random() * 10)%3;
+		for(int day=0; day<working_days; day++) {
+			if(totalHours>=max_hours) {
+				break;
+			}
+			switch(empCheck) {
+				case full_time:
+					empHours=8;
+					totalHours+=empHours;
+					break;
+				case part_time:
+					empHours=4;
+					totalHours+=empHours;
+					break;
+				default:
+					empHours=0;
+			}
+		
+		}
+		empWage=totalHours*emp_rate_per_hour;
+		return empWage;
+	}
+	public static void main(String[] args) {
+		System.out.println("Employee Wage for company ABC: "+computeWage(20, 20, 100));
+		System.out.println("Employee Wage for company XYZ: "+computeWage(20, 20, 100));
+	}
+}
